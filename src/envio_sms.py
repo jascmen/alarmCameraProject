92abc7aa815fc4ecc47598950e0eb9b2bfbedfa8
Jorge Anthony Sanchez Chavez <jascmen@gmail.com>
2024-05-14 13:04:51 -0500

    first commit

---
+++ src/envio_sms.py
@@ -0,0 +1,18 @@
+# envio_sms.py
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+def enviar_sms():
+    load_dotenv()
+
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body='cuidado hay un papu rondando tu laptop, revisa tu email para ver la imagen',
+        from_='+14356592185',
+        to='+51941768950'
+    )
